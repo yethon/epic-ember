39386f6656f2ab861c5a88fc9eaef54d5bb5ee53
yethon <ursula.yethon@mheducation.com>
2015-12-01 20:31:23 -0500

    Initialize Kinvey with your app information

--- app/services/kinvey-service.js
+++ app/services/kinvey-service.js
@@ -18,13 +18,25 @@ export default Ember.Service.extend({
     // These don't classify as an error, because you can still have fun pinging
     // the Kinvey service before you actually set up an app
     // see ../config/environment.js for more info about these variables
-    this.set('kinveyAppID', ENV.KINVEY_EPIC_EMBER_APP_ID);
-    this.set('kinveyAppID', ENV.KINVEY_EPIC_EMBER_APP_SECRET);
+    if(ENV.KINVEY_EPIC_EMBER_APP_ID && ENV.KINVEY_EPIC_EMBER_APP_SECRET) {
+      this.set('kinveyAppID', ENV.KINVEY_EPIC_EMBER_APP_ID);
+      this.set('kinveyAppSecret', ENV.KINVEY_EPIC_EMBER_APP_SECRET);
+      var options = {
+        appKey: this.get('kinveyAppID'),
+        appSecret: [this.get('kinveyAppSecret')]
+      };
+      this._initializeKinvey(options);
+    }
   }),
 
+  // If you don't have an app with Kinvey, this will return a generic message
   ping: function () {
     return this.get('kinveyAPI').ping();
-  }
+  },
 
-  // create something!
+  // create a user L3751
+
+  _initializeKinvey: function (options) {
+    return this.get('kinveyAPI').init(options);
+  }
 });
